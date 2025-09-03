681255c9354efc5217e54b22ff2b90cb1b78520f
Steph-hub2504 <stephanenana2504@gmail.com>
2025-09-03 22:21:52 +0000

    Ajout de section qui suit le header

--- src/components/Section1.tsx
+++ src/components/Section1.tsx
@@ -7,7 +7,7 @@ function Section1(){
             <h1>L'Intelligence Artificielle au service de l'Afrique</h1>
             <h2>Première DeepTech d'Afrique de l'Ouest, nous transformons les entreprises avec des solutions d'IA sur mesure</h2>
             <div className="btn">
-                <button className="commencer">Commencer votre transformation<i className="fa-solid fa-arrow-right"></i></button>
+                <button className="commencer">Commencer votre transformation  <i className="fa-solid fa-arrow-right"></i></button>
                 <button className="decouvrir">Découvrir notre expertise</button>
             </div>
          </div>
