b83dd1d91c3ed11e71feee49abc537477c2010bc
Steph-hub2504 <stephanenana2504@gmail.com>
2025-09-03 18:01:45 +0000

    Texte descriptif

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./components/Header";
+import Section1 from "./components/section1";
 
 function App() {
   return (
     <>
       <Header/>
+      <Section1/>
       <div>
         <h1>Hello Adam's</h1>
       </div>
---
+++ src/components/Section1.tsx
@@ -0,0 +1,20 @@
+
+
+function Section1(){
+
+    return (
+         <div className="ia-section">
+            <h1>L'Intelligence Artificielle au service de l'Afrique</h1>
+            <h2>Première DeepTech d'Afrique de l'Ouest, nous transformons les entreprises avec des solutions d'IA sur mesure</h2>
+            <div className="btn">
+                <button className="commencer">Commencer votre transformation<i className="fa-solid fa-arrow-right"></i></button>
+                <button className="decouvrir">Découvrir notre expertise</button>
+            </div>
+         </div>
+        
+         
+    );
+
+}
+
+export default Section1;
