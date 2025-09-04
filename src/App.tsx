114500a386cce8762a75ddfcdfa580c794fba1a7
Steph-hub2504 <stephanenana2504@gmail.com>
2025-09-04 19:08:48 +0000

    conception de la section qui presente les visions de l'entreprise

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./components/Header";
-import Section1 from "./components/section1";
+import Section1 from "./components/Section1";
+import Section2 from "./components/Section2";
 
 function App() {
   return (
     <>
       <Header/>
       <Section1/>
+      <Section2/>
       <div>
         <h1>Hello Adam's</h1>
       </div>
---
+++ src/components/Section2.tsx
@@ -0,0 +1,41 @@
+
+
+function Section2(){
+
+    return (
+        <div className="vision-section">
+         <h1 className="title-section">Notre Vision</h1>
+         <h2 className="subtitle-section">"The invisible is our playground"</h2>
+         <h3 className="description">Nous transformons l'invisible complexité opérationnelle en valeur tangible. Notre approche human-centric place l'humain au centre de la transformation digitale.</h3>
+         <div className="cards">
+            <div className="card">
+                <div className="circle">
+                 <i className="fa-solid fa-heart"></i>
+                </div>
+                <h3 className="smalltext">L'IA pour l'opérationnel</h3>
+                <h3 className="smalldescription">Nous automatisons les tâches répétitives pour libérer le potentiel créatif</h3>
+
+            </div>
+            <div className="card">
+                <div className="circle">
+                  <i className="fa-solid fa-users-line"></i>
+                </div>
+                <h3 className="smalltext">L'humain pour la valeur</h3>
+                <h3 className="smalldescription">L'intelligence humaine reste au cœur de la création de valeur stratégique</h3>
+
+            </div>
+            <div className="card">
+                <div className="circle">
+                  <i className="fa-solid fa-chart-line"></i>
+                </div>
+                <h3 className="smalltext">Augmenter, pas remplacer</h3>
+                <h3 className="smalldescription">Nos agents IA augmentent les capacités humaines pour une performance décuplée</h3>
+
+            </div>
+         </div>
+        </div>
+    );
+}
+
+
+export default Section2;
